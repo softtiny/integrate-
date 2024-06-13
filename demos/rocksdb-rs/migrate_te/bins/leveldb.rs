2df0a728795f05a74daaa870eb1114b8dc6bca6f
softtiny <yingxiancapatin@gmail.com>
2024-06-13 10:25:40 +0800

    :construction: Work in progress.

--- demos/rocksdb-rs/migrate_te/bins/leveldb.rs
+++ demos/rocksdb-rs/migrate_te/bins/leveldb.rs
@@ -1,41 +1,66 @@
-use rusty_leveldb::{compressor, CompressorId, DB, DBIterator, LdbIterator, Options};
-fn main() {
-    println!("Hello, world!");
-    // NB: db is automatically closed at end of lifetime
-	// let path = "_path_for_rocksdb_storage";
-	 // Get the command line arguments iterator
-	let mut args = std::env::args();
+use std::{borrow::Cow, path::PathBuf};
+
+use anyhow::{anyhow, bail, Result};
+use encoding_rs::{UTF_16LE, WINDOWS_1252};
+use rusty_leveldb::{CompressionType, Options, DB};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Wootility {
+    #[serde(rename = "_persist")]
+    pub persist: Value,
+    pub profiles: Value,
+    #[serde(rename = "wootilityConfig")]
+    pub wootility_config: Value,
+}
+
+impl Wootility {
+    
+    pub fn load(path: &str) -> Result<Self> {
+        let mut db = DB::open(
+            path,
+            Options {
+                compression_type: CompressionType::CompressionSnappy,
+                create_if_missing: false,
+                paranoid_checks: true,
+                ..Default::default()
+            },
+        )?;
+
+        const KEY: &[u8; 22] = b"_file://\x00\x01persist:root";
+        let encoded = db.get(KEY).ok_or(anyhow!("Couldn't find Wootility data"))?;
+        let decoded = Self::decode_string(&encoded)?;
+
+        Ok(serde_json::from_str(&decoded)?)
+    }
+
+    /// https://github.com/cclgroupltd/ccl_chrome_indexeddb
+    pub fn decode_string(bytes: &[u8]) -> Result<Cow<'_, str>> {
+        let prefix = bytes.first().ok_or(anyhow!("Invalid length"))?;
+        match prefix {
+            0 => Ok(UTF_16LE.decode(&bytes[1..]).0),
+            1 => Ok(WINDOWS_1252.decode(&bytes[1..]).0),
+            _ => bail!("Invalid prefix"),
+        }
+    }
+}
+
+fn main() -> Result<()> {
+	
+    let mut args = std::env::args();
 	 // Skip the first argument which is the program's name
 	let _ = args.next();
 	// Check if there is a first argument and print it
     if let Some(pathS) = args.next() {
         println!("First argument: {}", pathS);
 		let path = &pathS;
-		{
+        let wootility = Wootility::load(path)?;
+	    println!("{wootility:#?}");
 
-            let mut opt = Options::default();
-            opt.reuse_logs = false;
-            opt.reuse_manifest = false;
-            opt.compressor = compressor::SnappyCompressor::ID;
-            let mut db = DB::open(path, opt).unwrap();
-            match db.get(b"my key") {
-                Some(value) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
-                None => println!("value not found"),
-             }
-             match db.get(b"POSSIGNDATA") {
-                Some(value) => println!("POSSIGNDATA : {}", String::from_utf8(value).unwrap()),
-                None => println!("POSSIGNDATA value not found"),
-             }
-             match db.get(b"PERMISIONOCDE") {
-                 Some(value) => println!("PERMISIONOCDE : {}", String::from_utf8(value).unwrap()),
-                 None => println!("PERMISIONOCDE value not found"),
-             }
-             match db.get(b"LOCALSERVERURL") {
-                 Some(value) => println!("LOCALSERVERURL : {}", String::from_utf8(value).unwrap()),
-                 None => println!("LOCALSERVERURL value not found"),
-             } 
-        }
     } else {
         println!("No first argument provided.");
     }
+
+	Ok(())
 }
