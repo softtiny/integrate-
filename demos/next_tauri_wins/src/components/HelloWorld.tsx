4ec527cc4308c11d1fc44ae076e11611fe97d777
Kunkka <lxj@321tips.com>
2024-06-17 20:10:08 +0800

    :construction: Work in progress.

--- demos/next_tauri_wins/src/components/HelloWorld.tsx
+++ demos/next_tauri_wins/src/components/HelloWorld.tsx
@@ -1,7 +1,10 @@
 // components/HelloWorld.jsx
 import { useEffect } from "react";
+import { useClient } from 'next/client';
+
 
 const HelloWorld = () => {
+    useClient();
     useEffect(() => {
         window.alert(localStorage.getItem('complete'));
         localStorage.setItem('complete', Date.now().toString());
