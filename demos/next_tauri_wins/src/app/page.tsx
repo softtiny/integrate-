6344bb09167cde6552bf6de6446576bca44658db
Kunkka <lxj@321tips.com>
2024-06-17 19:32:34 +0800

    :construction: Work in progress.

--- demos/next_tauri_wins/src/app/page.tsx
+++ demos/next_tauri_wins/src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    window.alert(localStorage.getItem('complete'));
+    localStorage.setItem('complete', Date.now().toString());
+  }, []);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
