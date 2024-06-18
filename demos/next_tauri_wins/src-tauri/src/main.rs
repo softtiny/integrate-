f95625a04ecc1ae2b0f5cb189b88e0d2d6e6f621
softtiny <yingxiancapatin@gmail.com>
2024-06-18 14:31:44 +0800

    :construction: Work in progress.

--- demos/next_tauri_wins/src-tauri/src/main.rs
+++ demos/next_tauri_wins/src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 
 fn main() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_log::Builder::default().build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
