d41d336dae85fdc1117dd73374dbede1020d724a
softtiny <yingxiancapatin@gmail.com>
2024-06-20 15:24:53 +0800

    :construction: Work in progress.

---
+++ demos/next_capacitor_apk/src/app/components/HelloWorld.tsx
@@ -0,0 +1,23 @@
+'use client'
+// components/HelloWorld.jsx
+import { useEffect,useState } from "react";
+
+
+const HelloWorld = () => {
+    const [value, setValue] = useState('');
+    const [href, setHref] = useState('');
+
+    useEffect(() => {
+            setTimeout(()=>{
+
+                setValue(localStorage.getItem('complete')||"........");
+                localStorage.setItem('complete', Date.now().toString());
+                    },10000)
+                setHref(window.location.href);
+    }, []);
+    return (<div>
+        <div>{href}--Hello World!{value}</div>
+    </div>);
+};
+
+export default HelloWorld;
