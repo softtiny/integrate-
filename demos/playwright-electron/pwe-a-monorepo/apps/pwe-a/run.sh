3e2f6ab59a3909e84420dac2de21411de86d1937
softtiny <yingxiancapatin@gmail.com>
2025-06-19 17:05:43 +0800

    :construction: Work in progress.

--- demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
+++ demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
@@ -11,8 +11,8 @@ pwd
 rm -fr node_modules
 pnpm install --ignore-workspace
 node node_modules/electron/install.js
-sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
-sudo chmod 4755  node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
+sudo chown root:root node_modules/.pnpm/electron@37.0.0-beta.7/node_modules/electron/dist/chrome-sandbox
+sudo chmod 4755  node_modules/.pnpm/electron@37.0.0-beta.7/node_modules/electron/dist/chrome-sandbox
 #node_modules/.bin/electron || (pwd && ls /home/runner/work/integrate-/integrate-/demos/playwright-electron/pwe-a-monorepo/node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox && sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox  )
 
 pnpm exec playwright test --list example.spec.ts
