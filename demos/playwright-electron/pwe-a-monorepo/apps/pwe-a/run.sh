80dcbab51f88db4c419b547d9f24d681193e2642
softtiny <yingxiancapatin@gmail.com>
2025-06-19 15:39:32 +0800

    :construction: Work in progress.

--- demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
+++ demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
@@ -3,8 +3,6 @@ set -e
 cd demos/playwright-electron/pwe-a-monorepo/apps/pwe-a
 pwd
 
-sudo apt install -y imagemagick
-
 #rm -fr node_modules
 # node node_modules/electron/install.js
 # sudo chown root:root ../../node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
@@ -13,16 +11,13 @@ sudo apt install -y imagemagick
 rm -fr node_modules
 pnpm install --ignore-workspace
 node node_modules/electron/install.js
-TARGET_FIND=$(pnpm run where)
-TARGET=$(dirname $TARGET_FIND)
-echo $TARGET
-sudo chown root:root $TARGET/chrome-sandbox
-sudo chmod 4755  $TARGET/chrome-sandbox
+sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
+sudo chmod 4755  node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
 #node_modules/.bin/electron || (pwd && ls /home/runner/work/integrate-/integrate-/demos/playwright-electron/pwe-a-monorepo/node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox && sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox  )
 
 pnpm exec playwright test --list example.spec.ts
 echo ".........................................................................."
 echo $DISPLAY
 echo ".........................................................................."
-pnpm exec electron "--inspect=0" "--remote-debugging-port=0" "./demo_a/electron_main.mjs" & ( sleep 10 && import -window root screenshot.png && ls && exit)
-#pnpm exec playwright test example.spec.ts #--reporter json
+#pnpm exec electron "--inspect=0" "--remote-debugging-port=0" "./demo_a/electron_main.mjs" & ( sleep 10 && import -window root screenshot.png && ls && exit)
+pnpm exec playwright test example.spec.ts #--reporter json
