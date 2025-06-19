5894ebd73ae96364b9a344becb1c90a04d62a6b8
softtiny <yingxiancapatin@gmail.com>
2025-06-19 14:57:34 +0800

    :construction: Work in progress.

--- demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
+++ demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
@@ -2,6 +2,9 @@
 set -e
 cd demos/playwright-electron/pwe-a-monorepo/apps/pwe-a
 pwd
+
+sudo apt install -y imagemagick
+
 #rm -fr node_modules
 # node node_modules/electron/install.js
 # sudo chown root:root ../../node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
@@ -18,5 +21,5 @@ pnpm exec playwright test --list example.spec.ts
 echo ".........................................................................."
 echo $DISPLAY
 echo ".........................................................................."
-pnpm exec electron "--inspect=0" "--remote-debugging-port=0" "./demo_a/electron_main.mjs" & ( sleep 10 && xwd -root -display :0 -silent -out screenshot.xwd && ls && exit)
+pnpm exec electron "--inspect=0" "--remote-debugging-port=0" "./demo_a/electron_main.mjs" & ( sleep 10 && xwd -root -display :99 -silent -out screenshot.xwd && ls && exit)
 #pnpm exec playwright test example.spec.ts #--reporter json
