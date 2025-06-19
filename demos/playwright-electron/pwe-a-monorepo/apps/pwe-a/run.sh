e4c9c301574a6c8338be04bc2cbb5c90a851d3ed
softtiny <yingxiancapatin@gmail.com>
2025-06-19 11:21:49 +0800

    :construction: Work in progress.

--- demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
+++ demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
@@ -8,12 +8,12 @@ pwd
 #pnpm install --ignore-workspace
 #https://github.com/electron/electron/issues/20731 The path.txt is missing.
 node node_modules/electron/install.js
-node_modules/.bin/electron || (pwd && ls /home/runner/work/integrate-/integrate-/demos/playwright-electron/pwe-a-monorepo/node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox && sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox  )
-echo "llll................."
+#node_modules/.bin/electron || (pwd && ls /home/runner/work/integrate-/integrate-/demos/playwright-electron/pwe-a-monorepo/node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox && sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox  )
+#echo "llll................."
 #node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
 #node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
-sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
-sudo chmod 4755 node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
+sudo chown root:root ../../node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
+sudo chmod 4755  ../../node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
 node_modules/.bin/electron
 pnpm exec playwright test --list example.spec.ts
 pnpm exec electron
