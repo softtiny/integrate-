499927e0eb6c3ca40996ff11a048b51255049fee
softtiny <yingxiancapatin@gmail.com>
2025-06-19 15:19:00 +0800

    :construction: Work in progress.

--- demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
+++ demos/playwright-electron/pwe-a-monorepo/apps/pwe-a/run.sh
@@ -13,8 +13,11 @@ sudo apt install -y imagemagick
 rm -fr node_modules
 pnpm install --ignore-workspace
 node node_modules/electron/install.js
-sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
-sudo chmod 4755  node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox
+TARGET_FIND=$(pnpm run where)
+TARGET=$(dirname $TARGET_FIND)
+echo $TARGET
+sudo chown root:root $TARGET/chrome-sandbox
+sudo chmod 4755  $TARGET/chrome-sandbox
 #node_modules/.bin/electron || (pwd && ls /home/runner/work/integrate-/integrate-/demos/playwright-electron/pwe-a-monorepo/node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox && sudo chown root:root node_modules/.pnpm/electron@36.5.0/node_modules/electron/dist/chrome-sandbox  )
 
 pnpm exec playwright test --list example.spec.ts
